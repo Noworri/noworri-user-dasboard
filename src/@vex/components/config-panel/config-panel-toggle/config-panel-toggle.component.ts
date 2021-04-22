9792857d2845bc11bea1eaf3553a641f504de4f1
emericAz <azonsou158@gmail.com>
2021-04-22 08:38:11 +0100

    ui correction

--- src/@vex/components/config-panel/config-panel-toggle/config-panel-toggle.component.ts
+++ src/@vex/components/config-panel/config-panel-toggle/config-panel-toggle.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import icSettings from '@iconify/icons-ic/twotone-settings';
-
+import icArrow from '@iconify/icons-ic/twotone-arrow-right';
 @Component({
   selector: 'vex-config-panel-toggle',
   templateUrl: './config-panel-toggle.component.html',
@@ -11,6 +11,9 @@ export class ConfigPanelToggleComponent implements OnInit {
   @Output() openConfig = new EventEmitter();
 
   icSettings = icSettings;
+
+
+  icArrow=icArrow
 
   constructor() { }
 
